7f9e6851918901f5b52161addf3a620298ca07fe
Yaroslav <yorimark@abv.bg>
2019-11-29 14:03:42 +0200

    Second upload

--- main.tf
+++ main.tf
@@ -1,9 +1,24 @@
-resource "null_resource" "sleep" {
+# resource "null_resource" "sleep" {
 
 
-  provisioner "local-exec" {
-    # Bootstrap script called with private_ip of each node in the clutser
-    command = "sleep 300"
-  }
+#   provisioner "local-exec" {
+#     # Bootstrap script called with private_ip of each node in the clutser
+#     command = "sleep 300"
+#   }
+# }
+
+provider "aws" {
+  region     = "eu-west-2"
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
 
+resource "aws_instance" "ptfe" {
+
+  ami                    = "ami-0be057a22c63962cb"
+  instance_type          = "t2.micro"
+  tags = {
+    Name = "Yaro"
+  }
+
+  }
